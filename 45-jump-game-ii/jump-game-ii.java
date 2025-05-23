7040c2c7a86c95340aa9c5e12cb9fc875db904e3
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-05-23 09:12:49 +0530

    Time: 2 ms (31.60%) | Memory: 45 MB (73.85%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length<=1) return 0;
+        int maxReach = nums[0];
+        int steps = nums[0];
+        int jumpCount = 1;
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            steps--;
+
+            if (steps == 0) {
+                jumpCount++;
+                steps = maxReach - i;
+            }
+        }
+        return jumpCount++;
+    }
+}
