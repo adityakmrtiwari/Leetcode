0c66ba12277cedae98dd3805cc57c25c5a0df4de
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-07-29 23:57:26 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (9.65%) - LeetSync

--- 3207-make-three-strings-equal/make-three-strings-equal.java
+++ 3207-make-three-strings-equal/make-three-strings-equal.java
@@ -1,19 +1,17 @@
 class Solution {
     public int findMinimumOperations(String s1, String s2, String s3) {
-        int minL = Math.min(s1.length(),s2.length());
-        minL = Math.min(minL,s3.length());
+        int minL = Math.min(s1.length(), s2.length());
+        minL = Math.min(minL, s3.length());
 
-        int common =0;
-        for(int i=0;i<minL;i++){
-            if(s1.charAt(i)==s2.charAt(i) && s1.charAt(i)==s3.charAt(i))
+        int common = 0;
+        for (int i = 0; i < minL; i++) {
+            if (s1.charAt(i) == s2.charAt(i) && s1.charAt(i) == s3.charAt(i))
                 common++;
             else
                 break;
-            
         }
-
         if (common == 0)
             return -1;
-        return ((s1.length()-common) +(s2.length()-common) + (s3.length()-common));
+        return ((s1.length() - common) + (s2.length() - common) + (s3.length() - common));
     }
 }
