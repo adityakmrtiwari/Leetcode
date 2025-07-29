5d484e5b43a090a6603c4f61c11bf79cb81c9182
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-07-30 01:20:38 +0530

    Time: 44 ms (42.32%) | Memory: 53.6 MB (93.00%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,16 +1,16 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
-        Collections.sort(dictionary);
+        dictionary.sort((a, b) -> a.length() - b.length());
+        // Sorts strings by length in ascending order
         StringBuilder res = new StringBuilder();
         for (String str : sentence.split(" ")) {
-            String p = str;
             for (int i = 0; i < dictionary.size(); i++) {
                 if (str.startsWith(dictionary.get(i))) {
-                    p = dictionary.get(i);
+                    str = dictionary.get(i);
                     break;
                 }
             }
-            res.append(p+ " ");
+            res.append(str + " ");
         }
         return res.toString().trim();
     }
