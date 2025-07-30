10cafd151a5a35ae78a451cd6df2b9056653a142
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-07-31 00:31:35 +0530

    Time: 1 ms (99.48%) | Memory: 44.1 MB (52.39%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,18 +1,24 @@
 class Solution {
     public int compress(char[] chars) {
-        int read =0;
-        int write =0;
-        while(read<chars.length){
+
+        // Traverse the array with two pointers: read to scan and write to overwrite.
+        // For each group of repeated characters, write the character and its count (if > 1).
+        // Convert count to characters and write each digit to the array.
+
+        
+        int read = 0;
+        int write = 0;
+        while (read < chars.length) {
             int start = read;
-            while(read < chars.length && chars[read]==chars[start]){
+            while (read < chars.length && chars[read] == chars[start]) {
                 read++;
             }
-            chars[write]=chars[start];
+            chars[write] = chars[start];
             write++;
-            int ct=read-start;
-            if(ct>1){
-                for(char c: String.valueOf(ct).toCharArray()){
-                    chars[write]=c;
+            int ct = read - start;
+            if (ct > 1) {
+                for (char c : String.valueOf(ct).toCharArray()) {
+                    chars[write] = c;
                     write++;
                 }
             }
