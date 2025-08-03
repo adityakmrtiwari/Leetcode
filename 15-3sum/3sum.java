ddf02743fb54b739d87017c6ef04840dd1d3e415
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-08-04 01:07:11 +0530

    Time: 31 ms (56.74%) | Memory: 51.2 MB (96.91%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,37 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums); // Step 1: Sort the array
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue; // Skip duplicates
-
-            int target = -nums[i]; // Step 2: Find two numbers that sum to -nums[i]
-            int j = i + 1, k = nums.length - 1; // Two pointers
-
-            while (j < k) {
-                int sum = nums[j] + nums[k];
-
-                if (sum == target) {
-                    result.add(Arrays.asList(nums[i], nums[j], nums[k])); // Triplet found
-
-                    // Move both pointers while skipping duplicates
-                    while (j < k && nums[j] == nums[j + 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k - 1])
-                        k--;
-
-                    j++;
-                    k--;
-                } else if (sum < target) {
-                    j++; // Need a larger number
-                } else {
-                    k--; // Need a smaller number
-                }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int x = -nums[i];
+            int st = i+1;
+            int end = nums.length - 1;
+            while (st < end) {
+                if (nums[st] + nums[end] == x) {
+                    ans.add(Arrays.asList(nums[i],nums[st], nums[end]));
+                    st++;
+                    end--;
+                     while (st < end && nums[st] == nums[st - 1]) st++;
+                    while (st < end && nums[end] == nums[end + 1]) end--;
+                } else if (nums[st] + nums[end] > x)
+                    end--;
+                else if (nums[st] + nums[end] < x)
+                    st++;
             }
         }
-        return result;
-
+        return ans;
     }
 }
