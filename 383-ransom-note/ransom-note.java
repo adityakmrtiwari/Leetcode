d051a1440502269c7174d6cbfcc01d72e6618986
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-08-03 13:02:58 +0530

    Time: 3 ms (64.97%) | Memory: 44.7 MB (74.17%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,23 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> charCount = new HashMap<>();
-
-        // Count the frequency of each character in magazine
-        for (char c : magazine.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        int[] freq = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            freq[magazine.charAt(i) - 'a']++;
         }
 
-        // Check if ransomNote can be constructed
-        for (char c : ransomNote.toCharArray()) {
-            if (!charCount.containsKey(c) || charCount.get(c) == 0) {
+        for (int i = 0; i < ransomNote.length(); i++) {
+            freq[ransomNote.charAt(i) - 'a']--;
+            if (freq[ransomNote.charAt(i) - 'a'] < 0)
                 return false;
-            }
-            charCount.put(c, charCount.get(c) - 1);
         }
-        // - All characters of ransomNote have been checked and found in sufficient quantity
-        // - So, we return true
-
         return true;
-
     }
 }
