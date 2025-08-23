681d5cc34a692140270d191a2312df33cfb6be4b
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-08-24 00:16:46 +0530

    Time: 7 ms (99.98%) | Memory: 49.3 MB (87.38%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,19 +1,44 @@
 class NumArray {
-    int [] nums ;
+    int [] prefix ;
     public NumArray(int[] nums) {
-        this.nums=nums;
+        prefix= new int[nums.length];
+
+        prefix[0]=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
     }
     
     public int sumRange(int left, int right) {
-        int sum =0;
-
-        for(int i=left;i<=right;i++){
-            sum +=nums[i];
-        }
-        return sum;
+        if(left==0) return prefix[right];
+        return prefix[right] - prefix[left-1];
     }
 }
 
+
+
+// class NumArray {
+//     int [] nums ;
+//     public NumArray(int[] nums) {
+//         this.nums=nums;
+//     }
+    
+//     public int sumRange(int left, int right) {
+//         int sum =0;
+
+//         for(int i=left;i<=right;i++){
+//             sum +=nums[i];
+//         }
+//         return sum;
+//     }
+// }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
