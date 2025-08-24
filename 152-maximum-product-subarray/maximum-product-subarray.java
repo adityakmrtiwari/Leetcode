8e9bd315801c1b0fc4b7315468e094f89338f2a9
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-08-25 00:28:19 +0530

    Time: 2 ms (55.15%) | Memory: 45.1 MB (43.64%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -4,22 +4,20 @@ class Solution {
 
         int maxP = nums[0];
         int minP = nums[0];
-
-        int max = nums[0];
         int ans = nums[0];
 
-        for(int i=1;i<n;i++){
+        for (int i = 1; i < n; i++) {
 
-            if(nums[i]<0){
-                maxP = minP+maxP;
-                minP = maxP-minP;
-                maxP=maxP-minP;
+            if (nums[i] < 0) {
+                maxP = minP + maxP;
+                minP = maxP - minP;
+                maxP = maxP - minP;
             }
 
-            maxP = Math.max(maxP*nums[i],nums[i]);
-            minP = Math.min(minP*nums[i],nums[i]);
+            maxP = Math.max(maxP * nums[i], nums[i]);
+            minP = Math.min(minP * nums[i], nums[i]);
 
-            ans = Math.max(maxP,ans);
+            ans = Math.max(maxP, ans);
         }
         return ans;
     }
