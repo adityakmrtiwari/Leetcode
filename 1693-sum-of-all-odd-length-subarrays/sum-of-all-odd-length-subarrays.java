af0408bdeadd3581b4fce4b84bf79820f17b49b6
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-09-16 00:03:23 +0530

    Time: 2 ms (63.91%) | Memory: 41.7 MB (20.44%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.java
@@ -3,14 +3,33 @@ class Solution {
         int sum = 0;
         int n = arr.length;
         for (int i = 0; i < n; i++) {
-            for (int j = i ; j < n; j++) {
+            int s = 0;
+            for (int j = i; j < n; j++) {
+                s += arr[j];
                 if ((j - i + 1) % 2 == 1) {
-                    for (int k = i; k <= j; k++) {
-                        sum = sum + arr[k];
-                    }
+                    sum += s;
                 }
             }
         }
         return sum;
     }
 }
+
+// BRUTE FORCE ---> 0(N^3)
+
+// class Solution {
+//     public int sumOddLengthSubarrays(int[] arr) {
+//         int sum = 0;
+//         int n = arr.length;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i ; j < n; j++) {
+//                 if ((j - i + 1) % 2 == 1) {
+//                     for (int k = i; k <= j; k++) {
+//                         sum = sum + arr[k];
+//                     }
+//                 }
+//             }
+//         }
+//         return sum;
+//     }
+// }
