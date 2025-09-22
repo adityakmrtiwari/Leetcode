58c6664a3892e7af44d020639d52281a1b532940
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-09-23 01:06:28 +0530

    Time: 2 ms (75.55%) | Memory: 42.1 MB (86.58%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int num : nums) {
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        }
+
+        int max = 0;
+        for (int val : mp.values()) {
+            if (val > max)
+                max = val;
+        }
+        int ans = 0;
+        for (int val : mp.values()) {
+            if (val == max) {
+                ans = ans + val;
+            }
+        }
+        return ans;
+    }
+}
