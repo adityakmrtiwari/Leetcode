ca2cd3469792303c2bfa306817eccbd78196c4f5
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-10-02 00:10:09 +0530

    Time: 6 ms (25.65%) | Memory: 44.4 MB (95.26%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -4,15 +4,13 @@ class Solution {
         long ct = 0;
         int mod = 1_000_000_007;
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '1') {
+            if (s.charAt(i) == '1') {
                 ct++;
-            } else if (ch == '0' && ct != 0) {
-                ans = (ans + (ct + 1) * ct / 2) % mod;
-                ct = 0;
+                ans = (ans + ct) % mod;
+            }else{
+                ct=0;
             }
         }
-        ans = (ans + (ct + 1) * ct / 2) % mod;
         return (int) ans;
     }
 }
