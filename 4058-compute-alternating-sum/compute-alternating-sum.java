471624da195aee9abdcb5662e40f479852bb9054
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-10-07 15:25:44 +0530

    Time: 1 ms (95.24%) | Memory: 45 MB (6.48%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int oddSum = 0;
+        int evenSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0)
+                evenSum += nums[i];
+            else
+                oddSum += nums[i];
+        }
+        return evenSum - oddSum;
+    }
+}
