6fe20581b73899cda47c77a1de2edc2218768936
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-10-08 01:58:08 +0530

    Time: 29 ms (43.12%) | Memory: 61.9 MB (68.27%) - LeetSync

---
+++ 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean partitionArray(int[] nums, int k) {
+        int n = nums.length;
+
+        if (n % k != 0)
+            return false;
+
+        int groups = n / k;
+
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        for (int ct : freq.values()) {
+            if (ct > groups)
+                return false;
+        }
+
+        return true;
+    }
+}
