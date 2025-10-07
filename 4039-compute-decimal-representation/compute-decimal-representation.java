2c921af160087932eb2d2b0456dbf4ddde06cc1e
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-10-07 16:47:48 +0530

    Time: 2 ms (73.41%) | Memory: 43.2 MB (36.02%) - LeetSync

---
+++ 4039-compute-decimal-representation/compute-decimal-representation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] decimalRepresentation(int n) {
+        List<Integer> list = new ArrayList<>();
+        int temp = 1;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            if (lastDigit != 0)
+                list.add(lastDigit * temp);
+            n = n / 10;
+            temp = temp * 10;
+        }
+
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
