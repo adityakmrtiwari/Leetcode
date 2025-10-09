d657e005c6cd6447f337ee5958cdde93360515ad
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-10-09 23:29:44 +0530

    Time: 38 ms (22.61%) | Memory: 45.6 MB (35.71%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -2,14 +2,16 @@ class Solution {
     public String removeDuplicates(String s) {
         Stack<Character> st = new Stack<>();
 
-        for(int i=0;i<s.length();i++){
-            if(!st.isEmpty() && s.charAt(i) == st.peek()) st.pop();
-            else st.push(s.charAt(i));
+        for (int i = 0; i < s.length(); i++) {
+            if (!st.isEmpty() && s.charAt(i) == st.peek())
+                st.pop();
+            else
+                st.push(s.charAt(i));
         }
-        String t ="";
-        while(!st.isEmpty()){
-            t = st.pop() + t;
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
         }
-        return t;
+        return sb.reverse().toString();
     }
 }
