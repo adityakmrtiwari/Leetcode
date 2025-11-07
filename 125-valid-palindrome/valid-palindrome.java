b98addbf5eddd7159a2b68b5498a108e5a4b848d
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-11-08 02:55:39 +0530

    Time: 122 ms (7.58%) | Memory: 47.5 MB (5.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.trim();
-        String st = "";
 
+        String st = "";
         for (int i = 0; i < s.length(); i++) {
             if (Character.isLetterOrDigit(s.charAt(i))) {
                 st = st + s.charAt(i);
@@ -10,8 +9,8 @@ class Solution {
         }
         st = st.toLowerCase();
 
-        int l = 0, r = st.length() - 1;
-
+        int l = 0;
+        int r = st.length() - 1;
         while (l <= r) {
             if (st.charAt(l) != st.charAt(r))
                 return false;
