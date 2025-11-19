0ecc5ad611f1d692478ed20d35ea4a872a6d05e3
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-11-19 13:26:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (15.61%) - LeetSync

---
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int lar =0;
+        int sec =0;
+        int ind =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>lar){
+                sec = lar;
+                lar = nums[i];
+                ind = i;
+            }else if(nums[i]>sec && nums[i]<lar){
+                sec = nums[i];
+            }
+        }
+        if(lar>= 2* sec) return ind;
+        return -1;
+    }
+}
