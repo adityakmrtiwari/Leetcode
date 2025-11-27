7272238f5b78dee0fc648c014dbb1f04073ece58
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-11-27 15:35:21 +0530

    Time: 6 ms (97.81%) | Memory: 46.2 MB (44.68%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,33 +1,29 @@
 class Solution {
     public String removeKdigits(String num, int k) {
+        int n = num.length();
+        if (num == null || n == 0) return "0";
+        if (n <= k) return "0";
 
-        if(num.length()<=k)
-           return "0";
+        StringBuilder st = new StringBuilder();
 
-        Stack<Character> st = new Stack<>();
-        for (int i = 0; i < num.length(); i++) {
-          
-            while (!st.isEmpty() && num.charAt(i) < st.peek() && k > 0) {
-                st.pop();
+        for (char c : num.toCharArray()) {
+            while (k > 0 && st.length() > 0 && st.charAt(st.length() - 1) > c) {
+                st.deleteCharAt(st.length() - 1);
                 k--;
             }
-            st.push(num.charAt(i));
+            st.append(c);
         }
-        while (!st.isEmpty() && k > 0) {
-            st.pop();
+
+        while (k > 0) {
+            st.deleteCharAt(st.length() - 1);
             k--;
         }
-        String s = "";
-        while (!st.isEmpty()) {
-            s = st.pop() + s;
-        }
-        int m = 0;
-        while (m < s.length() && s.charAt(m) == '0') {
-            m++;
-        }
-        s = s.substring(m);
-        if(s.length()==0)
-           return "0";
-        return s;
+
+        int i = 0;
+        while (i < st.length() && st.charAt(i) == '0') i++;
+
+        String res = st.substring(i);
+
+        return res.length() == 0 ? "0" : res;
     }
 }
