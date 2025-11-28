ba355306e7821be1d8d5b3d43abc17d653bf1f1c
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-11-28 13:58:55 +0530

    Time: 1 ms (99.41%) | Memory: 43.6 MB (32.06%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.java
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.java
@@ -1,17 +1,21 @@
 class Solution {
     public int minOperations(List<Integer> nums, int k) {
+        int n = nums.size();
+
         boolean[] seen = new boolean[k + 1];
-        int ct = 0;
+
         int found = 0;
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            ct++;
+        int ops = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            ops++;
             if (nums.get(i) <= k && !seen[nums.get(i)]) {
                 seen[nums.get(i)] = true;
                 found++;
+                if (found == k)
+                    return ops;
             }
-            if (found == k)
-                return ct;
         }
-        return ct;
+        return ops;
     }
 }
