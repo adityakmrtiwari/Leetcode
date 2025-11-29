6c11493dad61f1572cc25fbc7e0b3fcff8beb6d2
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-11-29 17:35:27 +0530

    Time: 2 ms (49.78%) | Memory: 43.2 MB (11.51%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,34 +1,20 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        Stack<Character> sts = new Stack<>();
+        Stack<Character> stt = new Stack<>();
 
-        Stack<Character> st = new Stack<>();
-        Stack<Character> ts = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != '#') {
-                st.push(s.charAt(i));
-            } else if (s.charAt(i) == '#' && st.isEmpty())
-                continue;
-            else if (s.charAt(i) == '#')
-                st.pop();
+        for (char c : s.toCharArray()) {
+            if (c == '#') {
+                if (!sts.isEmpty()) sts.pop();
+            } else
+                sts.push(c);
         }
-
-        for (int i = 0; i < t.length(); i++) {
-            if (t.charAt(i) != '#') {
-                ts.push(t.charAt(i));
-            }  else if (t.charAt(i) == '#' && ts.isEmpty())
-                continue;
-            else if (t.charAt(i) == '#')
-                ts.pop();
+        for (char c : t.toCharArray()) {
+            if (c == '#') {
+                if (!stt.isEmpty()) stt.pop();
+            } else
+                stt.push(c);
         }
-
-        if (st.size() != ts.size())
-            return false;
-
-        while (!st.isEmpty()) {
-            if (st.pop() != ts.pop())
-                return false;
-        }
-        return true;
+        return stt.equals(sts);
     }
 }
