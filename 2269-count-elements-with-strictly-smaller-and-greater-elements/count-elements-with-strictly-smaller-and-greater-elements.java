07cf3fbeaa0bc79188b9c1d972d62f3e6fa11e84
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-12-01 00:26:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (35.85%) - LeetSync

--- 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.java
@@ -2,18 +2,15 @@ class Solution {
     public int countElements(int[] nums) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
             min = Math.min(min, num);
             max = Math.max(max, num);
         }
-        int res =0;
-        for(int val : map.keySet()){
-            if(val != min && val != max)
-                res = res + map.get(val);
+        int res = 0;
+        for (int num : nums) {
+            if (num > min && num < max)
+                res++;
         }
-
         return res;
     }
 }
