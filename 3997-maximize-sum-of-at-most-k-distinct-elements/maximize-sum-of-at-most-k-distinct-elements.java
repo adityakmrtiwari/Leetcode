2140d1742a11bebd214a86634c41075f938eb0df
Aditya Kumar Tiwari <126416792+adityakmrtiwari@users.noreply.github.com>
2025-12-17 01:06:08 +0530

    Time: 7 ms (78.65%) | Memory: 47.9 MB (9.57%) - LeetSync

--- 3997-maximize-sum-of-at-most-k-distinct-elements/maximize-sum-of-at-most-k-distinct-elements.java
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/maximize-sum-of-at-most-k-distinct-elements.java
@@ -1,8 +1,11 @@
 class Solution {
     public int[] maxKDistinct(int[] nums, int k) {
+
         Arrays.sort(nums);
+
         ArrayList<Integer> list = new ArrayList<>();
         int prev = Integer.MIN_VALUE;
+
         for (int i = nums.length - 1; i >= 0 && k > 0; i--) {
             if (nums[i] != prev) {
                 list.add(nums[i]);
@@ -10,6 +13,7 @@ class Solution {
                 prev = nums[i];
             }
         }
+
         int[] res = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             res[i] = list.get(i);
